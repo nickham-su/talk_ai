2bf13dfe310797e85b84c8e0660dbd6a3bee1110
suyun <50793247@qq.com>
2024-05-01 17:34:56 +0800

    fix: macos窗口无法恢复显示bug

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -9,15 +9,13 @@ class AppDelegate: FlutterAppDelegate {
 
   // 点击 dock 图标时，显示窗口
   override func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
-      if !flag {
-          for window in NSApp.windows {
-              if !window.isVisible {
-                  window.setIsVisible(true)
-              }
-              window.makeKeyAndOrderFront(self)
-              NSApp.activate(ignoringOtherApps: true)
-          }
+    for window in NSApp.windows {
+      if !window.isVisible {
+        window.setIsVisible(true)
       }
-      return true
+      window.makeKeyAndOrderFront(self)
+      NSApp.activate(ignoringOtherApps: true)
+    }
+    return true
   }
 }
