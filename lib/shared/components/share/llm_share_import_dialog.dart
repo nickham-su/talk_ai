034934e0800d4db1c8bf75510283ee70f453a990
suyun <nickham2.su@huolala.cn>
2024-05-24 11:04:03 +0800

    feat: 优化导入导出UI

--- lib/shared/components/share/llm_share_import_dialog.dart
+++ lib/shared/components/share/llm_share_import_dialog.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:math';
 
 import 'package:TalkAI/modules/llm/controllers/llm_share_controller.dart';
 import 'package:TalkAI/shared/components/snackbar.dart';
@@ -10,6 +11,7 @@ import '../../../modules/chat/repositorys/chat_app_repository.dart';
 import '../buttons/cancel_button.dart';
 import '../buttons/confirm_button.dart';
 import '../../services/llm_service.dart';
+import '../dialog_widget/dialog_widget.dart';
 import '../form_widget/text_widget.dart';
 
 class LLMShareImportDialog extends StatelessWidget {
@@ -21,88 +23,56 @@ class LLMShareImportDialog extends StatelessWidget {
   Widget build(BuildContext context) {
     String input = importValue ?? '';
 
-    return GetBuilder<LLMShareController>(
-      init: LLMShareController(),
-      builder: (controller) {
-        return Scaffold(
-          backgroundColor: Colors.transparent,
-          body: Center(
-            child: Container(
-              decoration: BoxDecoration(
-                color: Get.theme.scaffoldBackgroundColor,
-                borderRadius: BorderRadius.circular(8),
-                border: Border.all(
-                  color: Get.theme.colorScheme.outlineVariant,
+    return DialogWidget(
+      width: min(Get.width / 2, 400), // or whatever you need
+      height: 300,
+      title: '导入数据',
+      child: GetBuilder<LLMShareController>(
+        init: LLMShareController(),
+        builder: (controller) {
+          return Column(
+            children: [
+              Expanded(
+                child: TextWidget(
+                  labelText: '粘贴分享连接',
+                  hintText: '请在此粘贴分享连接',
+                  initialValue: input,
+                  maxLines: 100,
+                  isRequired: false,
+                  onChanged: (value) {
+                    input = value;
+                  },
                 ),
               ),
-              width: 500,
-              height: 300,
-              child: Column(
-                children: [
-                  Container(
-                    width: double.infinity,
-                    height: 48,
-                    alignment: Alignment.centerLeft,
-                    padding: const EdgeInsets.only(left: 16),
-                    decoration: BoxDecoration(
-                      border: Border(
-                        bottom: BorderSide(
-                          color: Get.theme.colorScheme.outlineVariant
-                              .withOpacity(0.5),
-                        ),
-                      ),
+              Container(
+                margin: EdgeInsets.only(top: 24),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ConfirmButton(
+                      text: '导入',
+                      onPressed: () async {
+                        try {
+                          await importData(input.trim());
+                        } catch (e) {
+                          snackbar('导入失败', '请检查分享链接是否正确！');
+                        }
+                      },
                     ),
-                    child: const Text(
-                      '导入数据',
-                      style: TextStyle(fontSize: 16),
+                    const SizedBox(width: 16),
+                    CancelButton(
+                      text: '取消',
+                      onPressed: () {
+                        Get.back();
+                      },
                     ),
-                  ),
-                  Expanded(
-                    child: Container(
-                      padding: const EdgeInsets.symmetric(horizontal: 24),
-                      child: TextWidget(
-                        labelText: '粘贴分享连接',
-                        hintText: '请在此粘贴分享连接',
-                        initialValue: input,
-                        maxLines: 6,
-                        isRequired: false,
-                        onChanged: (value) {
-                          input = value;
-                        },
-                      ),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.symmetric(vertical: 24),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        ConfirmButton(
-                          text: '导入',
-                          onPressed: () async {
-                            try {
-                              await importData(input.trim());
-                            } catch (e) {
-                              snackbar('导入失败', '请检查分享链接是否正确！');
-                            }
-                          },
-                        ),
-                        const SizedBox(width: 16),
-                        CancelButton(
-                          text: '取消',
-                          onPressed: () {
-                            Get.back();
-                          },
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ),
-        );
-      },
+            ],
+          );
+        },
+      ),
     );
   }
 
@@ -166,9 +136,6 @@ class LLMShareImportDialog extends StatelessWidget {
       ret += '助理：成功导入$countSuccess个';
       if (failureChatAppsList.isNotEmpty) {
         ret += '\n导入失败: ${failureChatAppsList.join('、')}';
-      }
-      if (countSuccess > 0) {
-        ret += '\n使用前，需要为助理设置模型！';
       }
     }
     snackbar('导入结果', ret, duration: const Duration(seconds: 5));
--- lib/shared/components/share/llm_share_link_dialog.dart
+++ lib/shared/components/share/llm_share_link_dialog.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:TalkAI/modules/llm/controllers/llm_share_controller.dart';
 import 'package:TalkAI/shared/components/snackbar.dart';
 import 'package:clipboard/clipboard.dart';
@@ -6,7 +8,7 @@ import 'package:get/get.dart';
 
 import '../buttons/cancel_button.dart';
 import '../buttons/confirm_button.dart';
-import '../../services/llm_service.dart';
+import '../dialog_widget/dialog_widget.dart';
 import '../form_widget/text_widget.dart';
 
 class LLMShareLinkDialog extends StatelessWidget {
@@ -16,6 +18,51 @@ class LLMShareLinkDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return DialogWidget(
+      width: min(Get.width / 2, 400), // or whatever you need
+      height: 300,
+      title: '分享',
+      child: GetBuilder<LLMShareController>(
+        init: LLMShareController(),
+        builder: (controller) {
+          return Column(
+            children: [
+              Expanded(
+                child: TextWidget(
+                  labelText: '复制发送给好友',
+                  initialValue: url,
+                  maxLines: 100,
+                  onChanged: (val) {},
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 24),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ConfirmButton(
+                      text: '复制',
+                      onPressed: () {
+                        FlutterClipboard.copy(url);
+                        snackbar('复制成功', '发送给好友吧');
+                      },
+                    ),
+                    const SizedBox(width: 16),
+                    CancelButton(
+                      text: '取消',
+                      onPressed: () {
+                        Get.back();
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          );
+        },
+      ),
+    );
+
     return GetBuilder<LLMShareController>(
       init: LLMShareController(),
       builder: (controller) {
