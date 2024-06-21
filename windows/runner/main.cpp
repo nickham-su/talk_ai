a1dd327568a1f1a6b90e52dbbee8d40ea97b2a72
nickham <50793247@qq.com>
2024-06-21 13:44:13 +0800

    windows update

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -9,7 +9,7 @@
 
 int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
                       _In_ wchar_t *command_line, _In_ int show_command) {
-  HWND hwnd = ::FindWindow(L"FLUTTER_RUNNER_WIN32_WINDOW", L"TalkAI");
+  HWND hwnd = ::FindWindow(L"FLUTTER_RUNNER_WIN32_WINDOW", L"Talk AI");
   if (hwnd != NULL) {
     DispatchToUniLinksDesktop(hwnd);
 
