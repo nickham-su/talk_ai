acc6a6d309b825bb9200fd62f6bdd6f1430add33
suyun <50793247@qq.com>
2024-07-24 21:45:02 +0800

    feat: 优化标题栏

--- lib/shared/components/layout/views/windows_title_bar/windows_title_bar.dart
+++ lib/shared/components/layout/views/windows_title_bar/windows_title_bar.dart
@@ -2,16 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:window_manager/window_manager.dart';
 
-import 'window_buttons.dart';
+import 'window_button.dart';
 
-class WindowsTitleBar extends StatelessWidget {
+class WindowsTitleBar extends StatefulWidget {
   const WindowsTitleBar({super.key});
+
+  @override
+  WindowsTitleBarState createState() => WindowsTitleBarState();
+}
+
+class WindowsTitleBarState extends State<WindowsTitleBar> {
+  /// 窗口置顶
+  bool isAlwaysOnTop = false;
+
+  /// 窗口最大化
+  bool isMaximized = false;
 
   @override
   Widget build(BuildContext context) {
     Offset? windowStartPosition;
     Offset? mouseStartPosition;
     return GestureDetector(
+      onDoubleTap: () {
+        toggleMaximized();
+      },
       onPanStart: (details) async {
         windowStartPosition = await windowManager.getPosition();
         mouseStartPosition = windowStartPosition! + details.localPosition;
@@ -33,18 +47,71 @@ class WindowsTitleBar extends StatelessWidget {
       child: Container(
           height: 32,
           color: Get.theme.colorScheme.secondaryContainer.withOpacity(0.3),
-          padding: const EdgeInsets.symmetric(horizontal: 8),
-          child: const Row(
+          padding: const EdgeInsets.only(left: 6),
+          child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Text(
-                'Talk AI',
-                style: TextStyle(fontSize: 14),
+              Row(
+                children: [
+                  SizedBox(
+                    width: 20,
+                    height: 20,
+                    child: Image.asset('assets/images/app_logo.png'),
+                  ),
+                  const Text(
+                    'Talk AI',
+                    style: TextStyle(fontSize: 14),
+                  ),
+                ],
               ),
-              WindowButtons()
+              Row(
+                children: [
+                  WindowButton(
+                    icon: 'assets/icons/pin.svg',
+                    onPressed: () {
+                      isAlwaysOnTop = !isAlwaysOnTop;
+                      windowManager.setAlwaysOnTop(isAlwaysOnTop);
+                      setState(() {});
+                    },
+                    checked: isAlwaysOnTop,
+                  ),
+                  WindowButton(
+                    icon: 'assets/icons/minus.svg',
+                    onPressed: () {
+                      windowManager.minimize();
+                    },
+                  ),
+                  WindowButton(
+                    icon: isMaximized
+                        ? 'assets/icons/window.svg'
+                        : 'assets/icons/square.svg',
+                    onPressed: () {
+                      toggleMaximized();
+                    },
+                  ),
+                  WindowButton(
+                    icon: 'assets/icons/close.svg',
+                    onPressed: () {
+                      windowManager.close();
+                    },
+                  ),
+                ],
+              ),
             ],
           )),
     );
+  }
+
+  /// 切换窗口最大化
+  void toggleMaximized() async {
+    if (await windowManager.isMaximized()) {
+      windowManager.unmaximize();
+      isMaximized = false;
+    } else {
+      windowManager.maximize();
+      isMaximized = true;
+    }
+    setState(() {});
   }
 }
