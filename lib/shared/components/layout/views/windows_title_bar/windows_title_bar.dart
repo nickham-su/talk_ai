44fa2470833c1a668099d75f84bf77fe01b86fb2
suyun <50793247@qq.com>
2024-07-24 21:57:15 +0800

    feat: 优化交互

--- lib/shared/components/layout/views/windows_title_bar/windows_title_bar.dart
+++ lib/shared/components/layout/views/windows_title_bar/windows_title_bar.dart
@@ -22,85 +22,97 @@ class WindowsTitleBarState extends State<WindowsTitleBar> {
   Widget build(BuildContext context) {
     Offset? windowStartPosition;
     Offset? mouseStartPosition;
-    return GestureDetector(
-      onDoubleTap: () {
-        toggleMaximized();
-      },
-      onPanStart: (details) async {
-        windowStartPosition = await windowManager.getPosition();
-        mouseStartPosition = windowStartPosition! + details.localPosition;
-      },
-      onPanUpdate: (details) async {
-        Offset windowPosition = await windowManager.getPosition();
-        if (windowStartPosition == null || mouseStartPosition == null) {
-          return;
-        }
-        Offset mousePosition = windowPosition + details.localPosition;
-        Offset windowCurrentPosition =
-            mousePosition - mouseStartPosition! + windowStartPosition!;
-        windowManager.setPosition(windowCurrentPosition);
-      },
-      onPanEnd: (details) {
-        windowStartPosition = null;
-        mouseStartPosition = null;
-      },
-      child: Container(
-          height: 32,
-          color: Get.theme.colorScheme.secondaryContainer.withOpacity(0.3),
-          padding: const EdgeInsets.only(left: 6),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Row(
-                children: [
-                  SizedBox(
-                    width: 20,
-                    height: 20,
-                    child: Image.asset('assets/images/app_logo.png'),
+    return Container(
+        height: 32,
+        color: Get.theme.colorScheme.secondaryContainer.withOpacity(0.3),
+        padding: const EdgeInsets.only(left: 6),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              child: GestureDetector(
+                onDoubleTap: () {
+                  toggleMaximized();
+                },
+                onPanStart: (details) async {
+                  windowStartPosition = await windowManager.getPosition();
+                  mouseStartPosition =
+                      windowStartPosition! + details.localPosition;
+                },
+                onPanUpdate: (details) async {
+                  Offset windowPosition = await windowManager.getPosition();
+                  if (windowStartPosition == null ||
+                      mouseStartPosition == null) {
+                    return;
+                  }
+                  Offset mousePosition = windowPosition + details.localPosition;
+                  Offset windowCurrentPosition = mousePosition -
+                      mouseStartPosition! +
+                      windowStartPosition!;
+                  windowManager.setPosition(windowCurrentPosition);
+                },
+                onPanEnd: (details) {
+                  windowStartPosition = null;
+                  mouseStartPosition = null;
+                },
+                child: Container(
+                  width: double.infinity,
+                  height: 32,
+                  alignment: Alignment.centerLeft,
+                  color: Colors.transparent,
+                  child: Row(
+                    children: [
+                      SizedBox(
+                        width: 20,
+                        height: 20,
+                        child: Image.asset('assets/images/app_logo.png'),
+                      ),
+                      const SizedBox(width: 2),
+                      const Text(
+                        'Talk AI',
+                        style: TextStyle(fontSize: 14),
+                      ),
+                    ],
                   ),
-                  const Text(
-                    'Talk AI',
-                    style: TextStyle(fontSize: 14),
-                  ),
-                ],
+                ),
               ),
-              Row(
-                children: [
-                  WindowButton(
-                    icon: 'assets/icons/pin.svg',
-                    onPressed: () {
-                      isAlwaysOnTop = !isAlwaysOnTop;
-                      windowManager.setAlwaysOnTop(isAlwaysOnTop);
-                      setState(() {});
-                    },
-                    checked: isAlwaysOnTop,
-                  ),
-                  WindowButton(
-                    icon: 'assets/icons/minus.svg',
-                    onPressed: () {
-                      windowManager.minimize();
-                    },
-                  ),
-                  WindowButton(
-                    icon: isMaximized
-                        ? 'assets/icons/window.svg'
-                        : 'assets/icons/square.svg',
-                    onPressed: () {
-                      toggleMaximized();
-                    },
-                  ),
-                  WindowButton(
-                    icon: 'assets/icons/close.svg',
-                    onPressed: () {
-                      windowManager.close();
-                    },
-                  ),
-                ],
-              ),
-            ],
-          )),
-    );
+            ),
+            Row(
+              children: [
+                WindowButton(
+                  icon: 'assets/icons/pin.svg',
+                  onPressed: () {
+                    isAlwaysOnTop = !isAlwaysOnTop;
+                    windowManager.setAlwaysOnTop(isAlwaysOnTop);
+                    setState(() {});
+                  },
+                  checked: isAlwaysOnTop,
+                ),
+                WindowButton(
+                  icon: 'assets/icons/minus.svg',
+                  onPressed: () {
+                    windowManager.minimize();
+                  },
+                ),
+                WindowButton(
+                  icon: isMaximized
+                      ? 'assets/icons/window.svg'
+                      : 'assets/icons/square.svg',
+                  onPressed: () {
+                    toggleMaximized();
+                  },
+                ),
+                WindowButton(
+                  icon: 'assets/icons/close.svg',
+                  onPressed: () {
+                    windowManager.close();
+                  },
+                ),
+              ],
+            ),
+          ],
+        ));
   }
 
   /// 切换窗口最大化
